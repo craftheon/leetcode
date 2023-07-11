be6d2b27d9bbcc9b903f35fd79a79a45ae199d3d
yuangwei <yuang.wya@gmail.com>
2023-07-11 14:02:02 +0800

    update solution

---
+++ playground/quick-sort.js
@@ -0,0 +1,14 @@
+// 快速排序代码实现
+
+/**
+ * 快速排序逻辑:
+ * 若要对 nums[lo..hi] 进行排序，我们先找一个分界点 p，
+ * 通过交换元素使得 nums[lo..p-1] 都小于等于 nums[p]，且 nums[p+1..hi] 都大于 nums[p]，
+ * 然后递归地去 nums[lo..p-1] 和 nums[p+1..hi] 中寻找新的分界点，最后整个数组就被排序了。
+ * @param {*} nums 
+ */
+function sort(nums) {
+
+
+}
+
