1f264a07795b45a5f8a644cf2fbe693625c1fffb
yuangwei <yuang.wya@gmail.com>
2023-08-16 19:01:37 +0800

    update solution

--- playground/tree.js
+++ playground/tree.js
@@ -7,14 +7,14 @@ function TreeNode(val, left, right) {
 
 
 
-const tree = new TreeNode(1,
-  new TreeNode(2,
-    new TreeNode(4),
-  ),
-  new TreeNode(5,
-    new TreeNode(5),
-  )
-)
+// const tree = new TreeNode(1,
+//   new TreeNode(2,
+//     new TreeNode(4),
+//   ),
+//   new TreeNode(5,
+//     new TreeNode(5),
+//   )
+// )
 
 function deleteTreeTarget(root, target) {
   if (root === null) return null
@@ -27,4 +27,30 @@ function deleteTreeTarget(root, target) {
   return root
 }
 
-console.log(JSON.stringify(deleteTreeTarget(tree, 5)))
+function buildTree(preorder, inorder) {
+  const build = function (arr) {
+    if (!arr.length) return null
+    const val = preorder.shift()
+    const idx = arr.indexOf(val)
+    const node = new TreeNode(val)
+    node.left = build(arr.slice(0, idx))
+    node.right = build(arr.slice(idx + 1))
+    return node
+  }
+  return build(inorder)
+}
+
+// var buildTree = function (preorder, inorder) {
+//   function build(arr) {
+//     if (!arr.length) return null;
+//     const val = preorder.shift();
+//     const idx = arr.indexOf(val);
+//     const node = new TreeNode(val);
+//     node.left = build(arr.slice(0, idx));
+//     node.right = build(arr.slice(idx + 1));
+//     return node;
+//   }
+//   return build(inorder);
+// };
+
+console.log(JSON.stringify(buildTree([3, 1, 2, 4, 5, 7], [2, 1, 4, 3, 7, 5])))
