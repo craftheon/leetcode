69424603f54dd6acceff51500cc830926e29665e
yuangwei <yuang.wya@gmail.com>
2023-08-24 16:10:12 +0800

    update solution

--- 167.two-sum-ii-input-array-is-sorted/solution.js
+++ 167.two-sum-ii-input-array-is-sorted/solution.js
@@ -1,8 +1,22 @@
 const assert = require('node:assert/strict')
 
-    // write code here..
+var twoSum = function (numbers, target) {
+    let n = numbers.length
+    let l = 0, r = n - 1
+    while (l < r) {
+        let sum = numbers[l] + numbers[r]
+        if (sum === target) {
+            return [l + 1, r + 1]
+        } else if (sum > target) {
+            r--
+        } else {
+            l++
+        }
+    }
+    return []
+};
 
-    (function () {
-        // test code here
-        console.log('Accepted!')
-    })()
+(function () {
+    // test code here
+    console.log('Accepted!')
+})()
