16084f27339b8ebeaaa8d87d5334ac971fa567e6
yuangwei <yuang.wya@gmail.com>
2023-08-25 14:52:43 +0800

    update solution

--- 1.two-sum/solution.go
+++ 1.two-sum/solution.go
@@ -1,10 +1,17 @@
 package main
 
-func twoSum(nums []int, target int) {
-
+func twoSum(nums []int, target int) []int{
+	hash := map[int]int{}
+	for k, v := range nums {
+		hash[v] = k
+		if c, ok := hash[target - v]; ok {
+      return []int{c, k}
+    }
+	}
+	return []int{}
 }
 
 
 func main() {
-
+	twoSum([]int{3, 2, 1, 4}, 9)
 }
